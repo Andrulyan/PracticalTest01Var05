fd367815ec007737e2d717c58e70036bc4cfe41b
Andrulyan <diniulian63@yahoo.com>
2024-11-13 08:50:24 +0200

    Task A

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
@@ -1,24 +1,41 @@
 package ro.pub.cs.systems.eim.practicaltest01var05;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class PracticalTest01Var05MainActivity extends AppCompatActivity {
+
+    private TextView displayTextView;
+    private Button buttonTopLeft, buttonTopRight, buttonCenter, buttonBottomLeft, buttonBottomRight, navigateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_practical_test01_var05_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Inițializarea elementelor UI
+        displayTextView = findViewById(R.id.displayTextView);
+        buttonTopLeft = findViewById(R.id.buttonTopLeft);
+        buttonTopRight = findViewById(R.id.buttonTopRight);
+        buttonCenter = findViewById(R.id.buttonCenter);
+        buttonBottomLeft = findViewById(R.id.buttonBottomLeft);
+        buttonBottomRight = findViewById(R.id.buttonBottomRight);
+        navigateButton = findViewById(R.id.navigateButton);
+
+        // Setarea funcționalității butoanelor
+        buttonTopLeft.setOnClickListener(v -> displayTextView.append("Top Left "));
+        buttonTopRight.setOnClickListener(v -> displayTextView.append("Top Right "));
+        buttonCenter.setOnClickListener(v -> displayTextView.append("Center "));
+        buttonBottomLeft.setOnClickListener(v -> displayTextView.append("Bottom Left "));
+        buttonBottomRight.setOnClickListener(v -> displayTextView.append("Bottom Right "));
+
+        // Setarea funcționalității pentru butonul de navigare
+        navigateButton.setOnClickListener(v -> {
+            Intent intent = new Intent(PracticalTest01Var05MainActivity.this, SecondaryActivity.class);
+            startActivity(intent);
         });
     }
 }
